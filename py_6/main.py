31484b677b1e941be44d30f7b01fde565f93dcae
oleg <olkud106@gmail.com>
2023-04-13 01:05:30 +0300

    asdasdas

--- py_6/main.py
+++ py_6/main.py
@@ -90,66 +90,164 @@
 # print(find_min_max(input_string))
 
 
-def find_median(input_string):
-    # Преобразую строку в список
-    input_string = input_string.split(', ')
-    if input_string:
-        try:
-            for i in range(len(input_string)):
-                input_string[i]=float(input_string[i])
-        except ValueError:
-            return 'Некорректный ввод'
-        
-        for n in range(len(input_string)):
-            for m in range(len(input_string)):
-                if input_string[n]>input_string[m]:
-                    input_string[n],input_string[m] = input_string[m],input_string[n]
-        # return input_string
-
-        if len(input_string)%2==0:
-            l = int(len(input_string)/2)
-            l = (input_string[l]+input_string[l-1])/2
-            return 'Median: {}'.format(l)
-        else:
-            l = int((len(input_string)-1)/2)
-            return 'Median: {}'.format(input_string[l])
+# def find_median(input_string):
+#     # Преобразую строку в список
+#     input_string = input_string.split(', ')
+#     # Если список пустой, тогда некорректный ввод
+#     if input_string:
+#         # Проверяю список на нечисловые значения и паралельно перевожу строки в числа
+#         try:
+#             for i in range(len(input_string)):
+#                 input_string[i]=float(input_string[i])
+#         except ValueError:
+#             return 'Некорректный ввод'
+#         # Сортирую пузырьком
+#         for n in range(len(input_string)):
+#             for m in range(len(input_string)):
+#                 if input_string[n]>input_string[m]:
+#                     input_string[n],input_string[m] = input_string[m],input_string[n]
+#         # Если в списке четное количество элементов, делю длину списка 
+#         # пополам, получаю индекс среднего числа смещенного вправо. Затем вычитаю из индекса 1 и получаю
+#         # среднее число смещенное влево. Нахожу среднее арифметическое этих чисел.
+#         if len(input_string)%2==0:
+#             l = int(len(input_string)/2)
+#             l = (input_string[l]+input_string[l-1])/2
+#             return 'Median: {}'.format(l)
+#         else:
+#         # Если в списке нечетное количество элементов. Нахожу количество элементов до среднего.
+#             l = int((len(input_string)-1)/2)
+#             return 'Median: {}'.format(input_string[l])
             
-    else:
-        return 'Некорректный ввод'
-# i = [1, 5, 2, 3, 6]
-# n = int((len(i)-1)/2)
-# print(i[n])
-# j = [1, 5, 2, 3, 6, 7,8,9]
-# print(j[len(j)])
+#     else:
+#         return 'Некорректный ввод'
+# # i = [1, 5, 2, 3, 6]
+# # n = int((len(i)-1)/2)
+# # print(i[n])
+# # j = [1, 5, 2, 3, 6, 7,8,9]
+# # print(j[len(j)])
 
-# input_string = input('Введите последовательность чисел: ')
-print(find_median(input_string='1, 5, 2, 3, 6'))
+# # input_string = input('Введите последовательность чисел: ')
+# print(find_median(input_string='1, 5, 2, 3, 6'))
+# # Ввод:
+# # 1, 5, 2, 3, 6
+
+# # Вывод:
+# # Median: 3.0
+# # ```
+# # ```
+# print(find_median(input_string='100, 5, 2, 4, 3, 6'))
+# # Ввод:
+# # 100, 5, 2, 4, 3, 6
+
+# # Вывод:
+# # Median: 4.5
+# # ```
+# # ```
+# # Ввод:
+# print(find_median(input_string=''))
+
+# # Вывод:
+# # Некорректный ввод
+# # ```
+# # ```
+# # Ввод:
+# print(find_median(input_string='десять, 10, пять, 7, семь'))
+# # десять, 10, пять, 7, семь
+
+# # Вывод:
+# # Некорректный ввод
+# # ```
+# def transform_string_to_integer(input_string):
+#     my_list = list()
+#     my_sum = 0
+#     input_string = input_string.split(' ')
+#     # Ищу число в словаре по первым буквам. 
+#     # Когда слово находится,добавляю в список и прерываю цикл(начинаю искать следующее значение/)
+#     for i in range(len(input_string)):
+#         for j in number_word_dict:
+#             if input_string[i].startswith(j):
+#                 my_list.append(number_word_dict[j])
+#                 break
+#                 # Если тысяча стоит в начале списка, тогда просто суммирую.
+#     if my_list[0]==1000:
+#             for n in range(len(my_list)):
+#                 my_sum+=my_list[n]
+                
+#     else:   
+#          for m in range(len(my_list)):
+#                 # Если тысяча стоит не в начале списка, тогда сумму полученную до 1000 умножаю на 1000.
+#                 if my_list[m]==1000:
+#                      my_sum*=1000
+#                      continue
+#                 my_sum+=my_list[m]
+        
+#     return my_sum
+
+# number_word_dict = {
+#     "ты": 1000, "м": 1000000,
+#     "сто": 100, "двес": 200, "трис": 300, "четырес": 400, "пятьс": 500, "шестьс": 600, "семьс": 700, "восемьс": 800, "девятьс": 900,
+#     "одинн": 11, "двен": 12, "трин": 13, "четырн": 14, "пятн": 15, "шестн": 16, "семн": 17, "восемн": 18, "девятн": 19,
+#     "двад": 20, "трид": 30, "сор": 40, "пятьд": 50, "шестьд": 60, "семьд": 70, "восемьд": 80, "девяно": 90,
+#     "деc": 10, "н": 0, "о": 1, "дв": 2, "т": 3, "ч": 4, "п": 5, "ш": 6, "с": 7, "в": 8, "д": 9, }
+
+# print(transform_string_to_integer('один'))
+# print(transform_string_to_integer('двадцать'))
+# print(transform_string_to_integer('двести сорок шесть'))
+# print(transform_string_to_integer('семьсот восемьдесят три тысячи девятьсот девятнадцать'))
+# print(transform_string_to_integer('девятьсот девяносто девять тысяч пятьсот шестьдесят три'))
+# # 1. Минимальное число — ноль (0) включительно. Максимальное число — один миллион (1000000) включительно.
+
+# 2. Гарантируется, что на вход подаются строки с описанием числа, проверять это не нужно.
+
+# **Пример №1:**
+# ```
 # Ввод:
-# 1, 5, 2, 3, 6
+# один
 
 # Вывод:
-# Median: 3.0
+# 1
 # ```
-# ```
-print(find_median(input_string='100, 5, 2, 4, 3, 6'))
-# Ввод:
-# 100, 5, 2, 4, 3, 6
 
-# Вывод:
-# Median: 4.5
-# ```
+# **Пример №2:**
 # ```
 # Ввод:
-print(find_median(input_string=''))
+# двадцать
 
 # Вывод:
-# Некорректный ввод
+# 20
 # ```
+
+# **Пример №3:**
 # ```
 # Ввод:
-print(find_median(input_string='десять, 10, пять, 7, семь'))
-# десять, 10, пять, 7, семь
+# двести сорок шесть
 
 # Вывод:
-# Некорректный ввод
+# 246
 # ```
+
+# **Пример №4:**
+
+# ```
+# Ввод:
+# семьсот восемьдесят три тысячи девятьсот девятнадцать
+
+# Вывод:
+# 783919
+import pandas as pd
+
+
+def delete_columns(df, col=[]):
+    
+    return df.drop(col,axis=1,inplace=True)
+
+
+customer_df = pd.DataFrame({
+            'number': [0, 1, 2, 3, 4],
+            'cust_id': [128, 1201, 9832, 4392, 7472],
+            'cust_age': [13, 21, 19, 21, 60],
+            'cust_sale': [0, 0, 0.2, 0.15, 0.3],
+            'cust_year_birth': [2008, 2000, 2002, 2000, 1961],
+            'cust_order': [1400, 14142, 900, 1240, 8430]
+        })
+# print(delete_columns(customer_df,[1,2]))
